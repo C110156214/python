889f4aecbd4ee16c0e9c9e67cbbdbd703af9ada6
COLA COCA <C110156214@nkust.edu.tw>
2022-03-22 21:49:01 +0800

    1

---
+++ 1.py
@@ -0,0 +1,19 @@
+# x= int(input("請輸入數字:"))
+# a=0
+# for i  in range(1,x+1):
+#     if x%i==0:
+#         a=a+1
+# if a==2:
+#     print(x,"是質數")
+# else:
+#     print(x,"不是質數")
+
+# c=[]
+# for j in str(x):
+#     c.append(int(j))
+# print(c)
+
+xx=48814
+for q in range(1,6):
+    for k in xx(1,6):
+        print(k)
